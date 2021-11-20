8240bffe48f1d87154685598c7c28ee0b44cb60e
David B <59053870+David-Beale@users.noreply.github.com>
2021-11-20 12:15:45 +0000

    add favourites adaptor

--- src/Components/Content/Components/Card/Card.js
+++ src/Components/Content/Components/Card/Card.js
@@ -1,9 +1,28 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectComicById } from "../../../../redux/comics";
+import {
+  addFavourite,
+  removeFavourite,
+  selectFavouriteById,
+} from "../../../../redux/favourites";
 import { Button, CardContainer, ComicCard, Image, Title } from "./CardStyle";
 
 export default function Card({ comicId }) {
+  const dispatch = useDispatch();
+
   const comic = useSelector((state) => selectComicById(state, comicId));
+  const favourite = useSelector((state) => selectFavouriteById(state, comicId));
+
+  const onClick = () => {
+    if (favourite) dispatch(removeFavourite(comicId));
+    else {
+      const payload = {
+        id: comic.id,
+        title: comic.title,
+      };
+      dispatch(addFavourite(payload));
+    }
+  };
   return (
     <CardContainer>
       <ComicCard>
@@ -12,7 +31,9 @@ export default function Card({ comicId }) {
           alt={`${comic.title} image`}
         />
         <Title>{comic.title}</Title>
-        <Button>Add to favourites</Button>
+        <Button onClick={onClick}>
+          {`${!favourite ? "Add to" : "Remove from"} favourites`}
+        </Button>
       </ComicCard>
     </CardContainer>
   );
--- src/redux/favourites.js
+++ src/redux/favourites.js
@@ -1,8 +1,11 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createEntityAdapter, createSlice } from "@reduxjs/toolkit";
 
-export const initialState = {
+const favouritesAdapter = createEntityAdapter();
+
+export const initialState = favouritesAdapter.getInitialState({
   open: false,
-};
+});
+
 const favourites = createSlice({
   name: "favourites",
   initialState,
@@ -10,9 +13,20 @@ const favourites = createSlice({
     toggleOpen(state) {
       state.open = !state.open;
     },
+    addFavourite(state, action) {
+      favouritesAdapter.addOne(state, action.payload);
+    },
+    removeFavourite(state, action) {
+      favouritesAdapter.removeOne(state, action.payload);
+    },
   },
 });
 
-export const { toggleOpen } = favourites.actions;
+export const { toggleOpen, addFavourite, removeFavourite } = favourites.actions;
+
+export const {
+  selectById: selectFavouriteById,
+  selectIds: selectFavouriteIds,
+} = favouritesAdapter.getSelectors((state) => state.favourites);
 
 export default favourites.reducer;
