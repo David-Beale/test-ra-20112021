ebdeaa217a2ab951199ce8433e766c29a3eb481a
David B <59053870+David-Beale@users.noreply.github.com>
2021-11-20 11:36:28 +0000

    Add comics slice and api

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -1,5 +1,11 @@
+import { useSelector } from "react-redux";
+import { selectAllComics } from "../../redux/comics";
 import { ContentContainer } from "./ContentStyle";
+import { useFetchComics } from "./Hooks/useFetchComics";
 
 export default function Content() {
+  useFetchComics();
+  const comics = useSelector(selectAllComics);
+  console.log(comics);
   return <ContentContainer></ContentContainer>;
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import comics from "./comics";
 import favourites from "./favourites";
 
 export default configureStore({
   reducer: {
+    comics,
     favourites,
   },
 });
